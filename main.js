ede81a04b67316899c4060ce13785945d3a9a21e
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-03-31 15:31:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
-const select = document.getElementById('selectRegions');
-let citiesList = [];
+import getSelectCities  from './selectCities.js'
+
+const select = document.getElementById('selectCities');
+
+const apiKey = '8b453078a3152e2271a388adfa37a904';
+const days = ['Воскресенье','Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const temperatureKelvin = 273;
+
+function displayWeather(){
+    select.addEventListener('change', () => {
+        citiesList.forEach(city => {
+            if (city.name === select.value) {
+                fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${city.lat}&lon=${city.lng}&appid=${apiKey}`)
+                    .then(response => response.json())
+                    .then(result => {
+                        for (let index = 0; index < 5; index++) {
+                            const currentDay = (day + index) > 6 ? (day + index - 7) : (day + index);
+                            document.getElementById(`dayWeek${index + 1}`).innerHTML = `${days[currentDay]}`;
+                            document.getElementById(`temperatureDay${index + 1}`).innerHTML = `Температура воздуха : ${Math.ceil(result.daily[index].temp.day - temperatureKelvin)} °С`;
+                            document.getElementById(`humidityDay${index + 1}`).innerHTML = `Влажность : ${result.daily[index].humidity} %`;
+                            document.getElementById(`windDay${index + 1}`).innerHTML = `Скорость ветра : ${Math.ceil(result.daily[index].wind_speed)} м/с`;
+                            (result.daily[index].weather).forEach(elem => {
+                                document.getElementById(`imgWeatherDay${index + 1}`).setAttribute('src', `http://openweathermap.org/img/wn/${elem.icon}@2x.png`);
+                            })
+                        }
+                    }
+                )
+            }
+        })
+    })
+}
+
+const day = new Date().getDay();
+let citiesList = getSelectCities();
+displayWeather();
