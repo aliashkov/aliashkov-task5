aa6781e63325de5c2291770a768845bce0e61f17
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-03-31 15:31:32 +0300

    Create selectCities.js

---
+++ selectCities.js
@@ -0,0 +1,21 @@
+const select = document.getElementById('selectCities');
+const url = 'https://gist.githubusercontent.com/alex-oleshkevich/6946d85bf075a6049027306538629794/raw/3986e8e1ade2d4e1186f8fee719960de32ac6955/by-cities.json';
+
+export default function getSelectCities() {
+    let citiesList = [];
+    fetch(url)
+        .then(response => response.json())
+        .then(result => result.forEach(element => {
+            element.regions.forEach(region => {
+                region.cities.forEach(city => {
+                    citiesList.push(city);
+                })
+            })
+            citiesList.map(city => city.name).sort().forEach(name => {
+                const opt = document.createElement('option');
+                select.appendChild(opt);
+                opt.innerHTML = name;
+            })
+        }));
+    return citiesList;
+}
