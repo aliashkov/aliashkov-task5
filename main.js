b767cbc6d382ac5a9d82d3d79d98b268f05da667
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-04-04 10:43:05 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
-import getSelectCities  from './selectCities.js'
+import getSelectCities from './selectCities.js'
 
 const select = document.getElementById('selectCities');
 
 const apiKey = '8b453078a3152e2271a388adfa37a904';
-const days = ['Воскресенье','Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 const temperatureKelvin = 273;
 
-function displayWeather(){
-    select.addEventListener('change', () => {
-        citiesList.forEach(city => {
-            if (city.name === select.value) {
-                fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${city.lat}&lon=${city.lng}&appid=${apiKey}`)
-                    .then(response => response.json())
-                    .then(result => {
-                       displayData(result)
-                    }
-                )
-            }
+function getWeather() {
+    try {
+        select.addEventListener('change', () => {
+            citiesList.forEach(city => {
+                if (city.name === select.value) {
+                    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${city.lat}&lon=${city.lng}&appid=${apiKey}`)
+                        .then(response => response.json())
+                        .then(result => {
+                            displayWeatherOnFiveDays(result)
+                        }
+                        )
+                }
+            })
         })
-    })
+    } catch (error) {
+        console.error('Ошибка:', error);
+    }
 }
 
-function displayData(result){
+function displayWeatherOnFiveDays(result) {
     for (let index = 0; index < 5; index++) {
+        document.getElementById("weather-forecast").hidden = false;
         const currentDay = (day + index) > 6 ? (day + index - 7) : (day + index);
         document.getElementById(`dayWeek${index + 1}`).innerHTML = `${days[currentDay]}`;
         document.getElementById(`temperatureDay${index + 1}`).innerHTML = `Температура воздуха : ${Math.ceil(result.daily[index].temp.day - temperatureKelvin)} °С`;
@@ -34,6 +39,7 @@ function displayData(result){
     }
 }
 
+document.getElementById("weather-forecast").hidden = true;
 const day = new Date().getDay();
 let citiesList = getSelectCities();
-displayWeather();
+getWeather();
